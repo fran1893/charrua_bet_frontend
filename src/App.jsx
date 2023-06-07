73dfbda1fc3f4b4537186e35589f03b49f8be6bf
Francisco Diaz <diazsalerno2@outlook.es>
2023-06-07 13:07:52 +0200

    Create component header and change body styles on index.scss

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import AppRouter from "./AppRouter";
-import "./App.scss";
 import { BrowserRouter } from "react-router-dom";
+import { Header } from "./components/";
+import "./App.scss";
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <Header />
         <AppRouter />
       </BrowserRouter>
     </>
