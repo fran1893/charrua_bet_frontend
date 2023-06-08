63916e5711a4446be70abe94608755bc7c3134d4
Francisco Diaz <diazsalerno2@outlook.es>
2023-06-08 12:48:11 +0200

    Update: Home component basic layout

--- src/containers/home/Home.jsx
+++ src/containers/home/Home.jsx
@@ -2,6 +2,14 @@ import React, { useEffect, useState } from "react";
 import teamService from "../../_services/teamService";
 import gameService from "../../_services/gamesService";
 import banner_image from "../../assets/banner_image.png";
+import diamond_logo from "../../assets/sports-nav-images/diamond-logo.png";
+import football_logo from "../../assets/sports-nav-images/football.png";
+import basketball_logo from "../../assets/sports-nav-images/basketball.png";
+import tenis_logo from "../../assets/sports-nav-images/tenis.png";
+import fifa_logo from "../../assets/sports-nav-images/FIFA.png";
+import american_football_logo from "../../assets/sports-nav-images/american-football.png";
+import baseball_logo from "../../assets/sports-nav-images/baseball.png";
+import esports_logo from "../../assets/sports-nav-images/esports.png";
 import "./Home.scss";
 
 export default function Home() {
@@ -43,29 +51,46 @@ export default function Home() {
   };
 
   return (
-    <div className="Home">
-      <div className="banner">
-        <img src={banner_image} alt="banner image" className="img-fluid" />
+    <div className="Home container">
+      <div className="sports-nav">
+        <p className="sports-nav-title"><img src={diamond_logo} alt="diamond logo" /> POPULAR</p>
+        <p><img src={football_logo} alt="diamond logo" /> Fútbol</p>
+        <p><img src={baseball_logo} alt="diamond logo" /> Baloncesto</p>
+        <p><img src={tenis_logo} alt="diamond logo" /> Tenis</p>
+        <p className="sports-nav-title"><img src={diamond_logo} alt="diamond logo" /> DEPORTES</p>
+        <p><img src={football_logo} alt="diamond logo" /> Fútbol</p>
+        <p><img src={fifa_logo} alt="diamond logo" /> FIFA</p>
+        <p><img src={american_football_logo} alt="diamond logo" /> Fútbol americano</p>
+        <p><img src={baseball_logo} alt="diamond logo" /> Beisbol</p>
+        <p><img src={esports_logo} alt="diamond logo" /> Esports</p>
       </div>
-      <div className="games container">
-        {games.map((game) => (
-          <div key={game.id} className="game-wrapper">
-            <div className="game">
-              <div className="home-team">
-                <div className="home-logo">
-                  <img src={game.home_logo} />
+      <div className="banner-feed">
+        <div className="banner">
+          <img src={banner_image} alt="banner image" className="img-fluid" />
+        </div>
+        <div className="games container">
+          {games.map((game) => (
+            <div key={game.id} className="game-wrapper">
+              <div className="game">
+                <div className="home-team">
+                  <div className="home-logo">
+                    <img src={game.home_logo} />
+                  </div>
+                  {game.home_team}
                 </div>
-                {game.home_team}
-              </div>
-              <div className="away-team">
-                {game.away_team}{" "}
-                <div className="away-logo">
-                  <img src={game.away_logo} />
+                <div className="away-team">
+                  {game.away_team}{" "}
+                  <div className="away-logo">
+                    <img src={game.away_logo} />
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
+      </div>
+      <div className="bet-sheet">
+        <p>HOJA DE APUESTAS</p>
       </div>
     </div>
   );
