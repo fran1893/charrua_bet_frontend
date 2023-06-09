800ec6f772998b2a7da6c8459f100a94587f45f1
Francisco Diaz <diazsalerno2@outlook.es>
2023-06-09 18:37:22 +0200

    Add: DataListTable component

---
+++ src/components/data-list-table/DataListTable.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import "./DataListTable.scss";
+import { MDBTable, MDBTableHead, MDBTableBody } from "mdb-react-ui-kit";
+
+export default function DataListTable({
+  data,
+  title = "Data",
+  headers,
+  attributes,
+  onChange,
+}) {
+  return (
+    <div className="DataListTable">
+      <div style={{ overflowX: "auto" }}>
+        <div className="mobileTableTitle">{title}</div>
+        <table>
+          <thead>
+            <tr>
+              <th colSpan={headers.length}>
+                <div className="tableTitle">{title}</div>
+              </th>
+            </tr>
+            <tr>
+              {headers.map((th, index) => (
+                <th scope="col" key={index}>
+                  {th}
+                </th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((d) => (
+              <tr scope="row" data-data-id={d.id} onClick={onChange} key={d.id}>
+                {attributes.map((attr, index) => (
+                  <td data-label={headers[index]} key={index}>
+                    {d[attr] ? d[attr] : "No definido"}
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
