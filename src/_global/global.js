e239f6afbd754a8dc8014237e37234d775cb2284
Francisco Diaz <diazsalerno2@outlook.es>
2023-11-17 10:50:04 +0100

    up

--- src/_global/global.js
+++ src/_global/global.js
@@ -1,4 +1,4 @@
 export const global = {
-    BASE_URL: "https://charrua-bet-backend-dev-nbhp.4.us-1.fl0.io",
+    BASE_URL: "https://charrua-bet-backend-dev-nbhp.4.us-1.fl0.io/",
     TOKEN_KEY: "auth-token",
   };
